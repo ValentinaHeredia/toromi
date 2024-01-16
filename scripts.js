88249b0d13d05f1953e0a8fe16d451ae4210e0d1
Valentina Heredia <valu.heredia05@gmail.com>
2024-01-16 15:39:02 -0300

    Cambio de precios en snacks

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// let nombre=prompt("Ingrese su nombre")
+// alert("Hola " + nombre)
+
+// let n1= 2023
+// let edad= parseInt(prompt("Ingrese edad")); /*ParseInt y Parsefloat convierten los prompt en un Int o Float*/
+// let nacimiento= n1-edad
+// console.log("Su año de nacimiento es: " + nacimiento)
+
+// let frase1= prompt("Ingrese la primera frase")
+// let frase2= prompt("Ingrese la segunda frase")
+// alert(frase1 +" " + frase2)
+
+let i;
+for (i=0 ; i<=20 ; i+=2){
+    console.log(i)
+}
