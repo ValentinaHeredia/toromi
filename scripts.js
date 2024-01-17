7ff9527fe8b77cef5ecd629b93e267cbbb96c38d
Valentina Heredia <valu.heredia05@gmail.com>
2024-01-17 13:48:29 -0300

    Navbar responsive

--- scripts.js
+++ scripts.js
@@ -10,7 +10,40 @@
 // let frase2= prompt("Ingrese la segunda frase")
 // alert(frase1 +" " + frase2)
 
-let i;
-for (i=0 ; i<=20 ; i+=2){
-    console.log(i)
+let num1;
+let num2;
+let opcion=prompt("Ingrese la opcion deseada= +, -, *, /, 0")
+
+while (opcion!= "0"){
+    do{
+        num1=parseInt(prompt("Ingrese un numero"))
+    }while (isNaN(num1));
+    do{
+        num2=parseInt(prompt("Ingrese un numero"))
+    }while (isNaN(num2));
+
+    switch (opcion){
+        case "+":
+            alert("El resultado de la suma es: "+ (num1+num2));
+            break;
+        case "-":
+            alert("El resultado de la resta es: "+ (num1-num2));
+            break;
+        case "*":
+            alert("El resultado de la multiplicación es: "+ (num1*num2))
+            break;
+        case "/":
+            if(num1==0 && num2==0){
+                alert("El resultado es indeterminado")
+            } else{
+                alert("El resultado de la division es :" + (num1/num2))
+            }
+            break;
+        case "0":
+            alert("ADIOS");
+            break;
+        default:
+            alert("Opcion incorrecta");
+    }
+    opcion=prompt("Ingrese la opcion deseada= +, -, *, /, 0")
 }
